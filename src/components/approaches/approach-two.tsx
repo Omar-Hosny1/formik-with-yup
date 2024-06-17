5b1384eb323eefa8b912c706422464506f4eb61c
Omar Hosny <102857390+Omar-Hosny1@users.noreply.github.com>
2024-06-18 01:10:45 +0300

    feat(validation): added second from validation approach

--- src/components/approaches/approach-two.tsx
+++ src/components/approaches/approach-two.tsx
@@ -1,68 +1,165 @@
 import React from 'react';
-import { Box,Input, Button } from '@chakra-ui/react';
+import { Box, Button, useToast } from '@chakra-ui/react';
+import { ErrorMessage, Field, Formik, FormikHelpers } from 'formik';
+import ApproachTwoValidationSchema from '../../validations/approach-two-validations';
 
 interface Props {
-  onSubmit: () => void;
+  afterSubmit: () => void;
   onBack: () => void;
 }
 
-function ApproachTwo({ onSubmit, onBack }: Props) {
+interface FistApproachProps {
+  username: string;
+  email: string;
+  password: string;
+  gender: string;
+  age: number;
+}
+
+const fieldStyles = {
+  padding: '8.5px 16px',
+  borderWidth: '1px',
+  transition: '0.3s',
+  borderColor: 'black',
+  borderRadius: '0.375rem',
+};
+
+const initialValues: FistApproachProps = {
+  username: '',
+  email: '',
+  password: '',
+  gender: '',
+  age: 0,
+};
+
+function ApproachTwo({ afterSubmit, onBack }: Props) {
+  const toast = useToast();
+
+  const onSubmit = (
+    values: FistApproachProps,
+    formik: FormikHelpers<FistApproachProps>
+  ) => {
+    formik.validateForm();
+    console.log(values);
+    toast({
+      title: 'Submitted Successfully',
+      status: 'success',
+      description: 'Forwarding To The Last Form (Approach)',
+    });
+    afterSubmit();
+  };
+
   return (
-    <Box
-      width={'fit-content'}
-      padding={'10px'}
-      display={'flex'}
-      m={'10px'}
-      justifyContent={' '}
-      borderRadius={'10px'}
-      alignItems={' '}
-      border={'1px solid black'}
+    <Formik
+      onSubmit={onSubmit}
+      validationSchema={ApproachTwoValidationSchema}
+      initialValues={initialValues}
     >
-      <Box
-        bg={'white'}
-        padding={'15px'}
-        borderRadius={'10px'}
-        display={'flex'}
-        justifyContent={' '}
-        alignItems={'stretch'}
-        flexDir={'column'}
-        gap={'10px'}
-        w={'500px'}
-      >
-        <Input borderColor={'black'} placeholder="User Name" />
-        <Input borderColor={'black'} placeholder="Email" />
-        <Input borderColor={'black'} placeholder="Password" />
-        <Input borderColor={'black'} placeholder="Gender" />
-        <Input borderColor={'black'} placeholder="Age" />
-        <Button
-          bg={'#3182ce'}
-          _hover={{
-            bg: '#3182ce',
-          }}
-          color={'white'}
-          type='submit'
-          onClick={() => {
-            onSubmit();
-          }}
+      {(formik) => (
+        <Box
+          width={'fit-content'}
+          padding={'10px'}
+          display={'flex'}
+          m={'10px'}
+          justifyContent={'center'}
+          borderRadius={'10px'}
+          alignItems={'center'}
+          border={'1px solid black'}
         >
-          Submit
-        </Button>
-        <Button
-        bg={"white"}
-          border={"1px solid #3182ce"}
-          _hover={{
-            bg: 'white',
-          }}
-          color={'#3182ce'}
-          type='submit'
-          onClick={() => {
-            onBack();
-          }}
-        >
-          Back 
-        </Button>
-      </Box>
-    </Box>
+          <Box
+            bg={'white'}
+            padding={'15px'}
+            borderRadius={'10px'}
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'stretch'}
+            flexDir={'column'}
+            gap={'10px'}
+            w={'500px'}
+          >
+            <label htmlFor="user">User Name</label>
+            <Field
+              className="hover-input"
+              name="username"
+              placeholder="User Name"
+              style={fieldStyles}
+            />
+            <ErrorMessage component={'span'} className="red" name="username" />
+
+            <label htmlFor="email">Email</label>
+            <Field
+              className="hover-input"
+              name="email"
+              placeholder="Email"
+              type="email"
+              style={fieldStyles}
+            />
+            <ErrorMessage component={'span'} className="red" name="email" />
+
+            <label htmlFor="password">Password</label>
+            <Field
+              className="hover-input"
+              name="password"
+              placeholder="Password"
+              type="password"
+              style={fieldStyles}
+            />
+            <ErrorMessage component={'span'} className="red" name="password" />
+
+            <label htmlFor="gender">Gender</label>
+            <Field
+              as="select"
+              className="hover-input"
+              name="gender"
+              style={fieldStyles}
+            >
+              <option>Please Enter Your Gender</option>
+              <option>Male</option>
+              <option>Female</option>
+            </Field>
+            <ErrorMessage component={'span'} className="red" name="gender" />
+
+            <label htmlFor="age">Age</label>
+            <Field
+              className="hover-input"
+              name="age"
+              placeholder="Age"
+              type="number"
+              style={fieldStyles}
+            />
+            <ErrorMessage component={'span'} className="red" name="age" />
+
+            <Button
+              bg={'#3182ce'}
+              _hover={{
+                bg: '#3182ce',
+              }}
+              color={'white'}
+              type="submit"
+              onClick={() => {
+                formik.handleSubmit();
+              }}
+            >
+              Submit
+            </Button>
+            <Button
+              bg={'white'}
+              border={'1px solid #3182ce'}
+              _hover={{
+                bg: 'white',
+              }}
+              color={'#3182ce'}
+              type="submit"
+              onClick={() => {
+                onBack();
+              }}
+            >
+              Back
+            </Button>
+          </Box>
+        </Box>
+      )}
+    </Formik>
   );
 }
 
---
+++ src/validations/approach-two-validations.ts
@@ -0,0 +1,38 @@
+import * as Yup from 'yup';
+
+const ApproachTwoValidationSchema = Yup.object({
+  username: Yup.string()
+    .trim()
+    .max(15, 'Your User Name Must my 15 Char as Maximum')
+    .required('Please Enter Your User Name')
+    .min(0, "This Field Can't Be Empty"),
+  email: Yup.string()
+    .trim()
+    .email('Please Enter a Valid Email')
+    .required('Please Enter Your Email')
+
+    .min(0, "This Field Can't Be Empty"),
+  password: Yup.string()
+    .trim()
+    .min(8, 'Password must be at least 8 characters long')
+    .max(20, 'Password must be no more than 20 characters long')
+    .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
+    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .matches(/\d/, 'Password must contain at least one number')
+    .matches(
+      /[@$!%*?&]/,
+      'Password must contain at least one special character'
+    )
+    .required('Password is required'),
+  gender: Yup.string()
+    .trim()
+    .required('Please Enter Your Gender')
+    .oneOf(['Male', 'Female']),
+
+  age: Yup.number()
+    .default(0)
+    .max(100, "Maximum Age Cant't be more than 100")
+    .min(10, "Minimum Age Cant't be less than 10"),
+});
+
+export default ApproachTwoValidationSchema;
